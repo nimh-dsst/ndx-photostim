21c1b0bd3e45ad6f8a477a5124162938b909dfad
carlwharris <50850205+carlwharris@users.noreply.github.com>
2022-09-28 15:42:37 -0400

    slm, device, photostimulation containers

--- src/pynwb/ndx_photostim/__init__.py
+++ src/pynwb/ndx_photostim/__init__.py
@@ -1,9 +1,12 @@
 import os
 from pynwb import load_namespaces, get_class
-from pynwb.core import NWBContainer
 from hdmf.utils import docval, get_docval, popargs
 from pynwb import register_class, load_namespaces
 from collections.abc import Iterable
+from pynwb.core import NWBContainer, NWBDataInterface
+from pynwb.device import Device
+from hdmf.utils import docval, popargs, get_docval, get_data_shape, popargs_to_dict
+
 
 # Set path of the namespace.yaml file to the expected install location
 ndx_photostim_specpath = os.path.join(
@@ -28,6 +31,66 @@ load_namespaces(ndx_photostim_specpath)
 # TODO: import your classes here or define your class using get_class to make
 # them accessible at the package level
 # SpatialLightModulator = get_class('SpatialLightModulator', 'ndx-photostim')
+register_class('SpatialLightModulator', 'ndx-photostim')
+class SpatialLightModulator(NWBContainer):
+
+    __nwbfields__ = ('dimensions',)
+
+    @docval(
+        {'name': 'name', 'type': str, 'doc': 'name of spatial light modulator'},
+        {'name': 'dimensions', 'type': Iterable, 'doc': 'dimensions ([w, h] or [w, h, d]) of SLM field'}
+    )
+    def __init__(self, **kwargs):
+        dimensions = popargs('dimensions', kwargs)
+        super().__init__(**kwargs)
+        self.dimensions = dimensions
+
+@register_class('PhotostimulationDevice', 'ndx-photostim')
+class PhotostimulationDevice(Device):
+
+    __nwbfields__ = ('type', 'wavelength', 'slm')
+
+    @docval(*get_docval(Device.__init__) + (
+        {'name': 'type', 'type': str, 'doc': 'type of stimulation (laser or LED)', 'default': None},
+        {'name': 'wavelength', 'type': float, 'doc': 'wavelength of photostimulation', 'default': None},
+        {'name': 'slm', 'type': SpatialLightModulator, 'doc': 'spatial light modulator', 'default': None},
+        ))
+    def __init__(self, **kwargs):
+        keys_to_set = ("type",
+                       "wavelength",
+                       "slm")
+        args_to_set = popargs_to_dict(keys_to_set, kwargs)
+        super().__init__(**kwargs)
+
+        for key, val in args_to_set.items():
+            setattr(self, key, val)
+
+
+@register_class('Photostimulation', 'ndx-photostim')
+class Photostimulation(NWBDataInterface):
+
+    # __nwbfields__ = ('device', 'roi_coordinates', 'stimulation_diameter')
+
+    @docval(*get_docval(NWBDataInterface.__init__) + (
+        {'name': 'device', 'type': PhotostimulationDevice, 'doc': 'photostimulation device'},
+        {'name': 'roi_coordinates', 'type': Iterable, 'doc': '[n,2] or [n,3] list of coordinates', 'default': None},
+        {'name': 'stimulation_diameter', 'type': (int, float), 'doc': 'diameter of stimulation (pixels)', 'default': None},
+        {'name': 'roi_mask', 'type': 'array_data', 'doc': 'pixel mask for ROI', 'default': None},
+        {'name': 'opsin', 'type': str, 'doc': 'opsin used', 'default': None},
+        {'name': 'peak_pulse_power', 'type': (int, float), 'doc': 'peak pulse power (J)', 'default': None}
+        ))
+    def __init__(self, **kwargs):
+        keys_to_set = ("device", "roi_coordinates",
+                       "stimulation_diameter", "roi_mask", "opsin", "peak_pulse_power")
+        args_to_set = popargs_to_dict(keys_to_set, kwargs)
+        super().__init__(**kwargs)
+
+        for key, val in args_to_set.items():
+            setattr(self, key, val)
+
+        if (self.roi_coordinates is None and self.stimulation_diameter is None) and (self.roi_mask is None):
+            raise TypeError("roi_coordinates & stimulation_diameter OR roi_mask must be specified")
+
 
 # @register_class('SpatialLightModulator' 'ndx-photostim')
 # class SpatialLightModulator(NWBContainer):
--- src/spec/create_extension_spec.py
+++ src/spec/create_extension_spec.py
@@ -26,38 +26,48 @@ def main():
     # included.
     # ns_builder.include_type('ElectricalSeries', namespace='core')
     ns_builder.include_type('NWBContainer', namespace='core')
+    ns_builder.include_type('Device', namespace='core')
+    ns_builder.include_type('NWBDataInterface', namespace='core')
 
     # TODO: define your new data types
     # see https://pynwb.readthedocs.io/en/latest/extensions.html#extending-nwb
     # for more information
-    tetrode_series = NWBGroupSpec(
+    slm = NWBGroupSpec(
         neurodata_type_def='SpatialLightModulator',
         neurodata_type_inc='NWBContainer',
-        doc=('slm'),
+        doc=('Spatial light modulator used in holographic photostimulation'),
         attributes=[
-            NWBAttributeSpec(
-                # name='size',
-                doc='vertices for surface, points in 3D space',
-                shape=(2, ),
-                name='size', dtype='float'
-            )
-        ],
-    )
+            NWBAttributeSpec('dimensions', 'dimensions ([w, h] or [w, h, d]) of SLM field', 'numeric',
+                             shape=((2,), (3,)),
+                             )
+        ])
 
-    ext = NWBGroupSpec(
+    photostim_device = NWBGroupSpec(
         neurodata_type_def='PhotostimulationDevice',
-        neurodata_type_inc='NWBContainer',
+        neurodata_type_inc='Device',
         doc=('photostimDevice'),
         attributes=[
-            NWBAttributeSpec(
-                # name='size',
-                name='slm',
-                doc='slm',
-                dtype=RefSpec('GroupSpec', 'object')
-            )
+            NWBAttributeSpec('type', 'type of stimulation (laser or LED)', 'text'),
+            NWBAttributeSpec('wavelength', 'wavelength of photostimulation', 'numeric'),
+            NWBAttributeSpec('slm', 'spatial light modulator device', RefSpec('GroupSpec', 'object'))
         ])
+
+    photostim = NWBGroupSpec(
+        neurodata_type_def='Photostimulation',
+        neurodata_type_inc='NWBDataInterface',
+        doc=('photostimulation container'),
+        attributes=[
+            NWBAttributeSpec('device', 'photostimulation device', RefSpec('GroupSpec', 'object')),
+            NWBAttributeSpec('roi_coordinates', '[n,2] or [n,3] list of coordinates', 'numeric',
+                             shape=((None, 2), (None, 3))),
+            NWBAttributeSpec('stimulation_diameter', 'diameter of stimulation (pixels)', 'numeric'),
+            NWBAttributeSpec('roi_mask', 'mask of region of interest', 'numeric'),
+            NWBAttributeSpec('opsin', 'opsin used', 'text'),
+            NWBAttributeSpec('peak_pulse_power', 'peak pulse power (J)', 'numeric')
+        ])
+
     # TODO: add all of your new data types to this list
-    new_data_types = [tetrode_series, ext]
+    new_data_types = [slm, photostim_device, photostim]
 
     # export the spec to yaml files in the spec folder
     output_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'spec'))
